547401a3ac14f6983e3a3ebd078f12d12af9efcb
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-10 15:02:57 +0200

    chore: add basic output logic

---
+++ PrintingApp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace PrintingApp
+{
+    /// <summary>
+    /// Basic C# program that demonstrates how to print output to the console using different formatting techniques.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Num = {0}", num);    // placeholder
+            Console.WriteLine($"Num = {num}");      // interpolation
+        }
+    }
+}
