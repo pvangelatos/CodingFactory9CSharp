4b9fd813312ca414e2b146766dfa4a0e2ff274a2
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-10 15:01:39 +0200

    chore: add hello world

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace HelloWorld
+﻿
+
+namespace HelloWorld
 {
+    /// <summary>
+    /// This is the first program in C# that prints "Hello, World!" to the console.
+    /// It serves as a simple introduction to C# programming and demonstrates the basic structure of a C# application.
+    /// </summary>
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine();
         }
     }
 }
