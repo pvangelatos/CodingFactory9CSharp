57bf15186bdb4713a1ddee2f88c18c3b263b1afd
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-15 10:40:20 +0200

    chore; add expression body methods

---
+++ ExpressionBodyMethod/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ExpressionBodyMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        // Expression-bodied method - concise syntax for methods
+        // that consist of a single expression.
+        public static int Add(int a, int b) => a + b;
+
+        public static int Factorial(int n) => (n == 0) ? 1 : n * Factorial(n - 1);
+
+        public static bool IsEven(int n) => n % 2 == 0;
+    }
+}
