2fcab9875fb0fa929418570386c9a0dd1a34665a
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-15 11:47:43 +0200

    chore: add string builder logic

---
+++ StringBuilderApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace StringBuilderApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----- String Ellapsed Time -------");
+            Console.WriteLine(DateTime.Now);
+            string collector = "Numbers: ";
+            for (int i = 1; i < 100000; i++)
+            {
+                collector += i;     // inefficient string concatenation
+            }
+            Console.WriteLine(collector.Substring(0, 20));
+            Console.WriteLine(DateTime.Now);
+
+            // StringBuilder is a mutable sequence of characters
+
+            Console.WriteLine("----- String Builder Ellapsed Time -------");
+            Console.WriteLine(DateTime.Now);
+            StringBuilder sb = new StringBuilder("Numbers: ");
+            for (int i = 1; i < 100000; i++)
+            {
+                sb.Append(i);    // efficient string building
+            }
+            Console.WriteLine(sb.ToString().Substring(0, 20));
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
