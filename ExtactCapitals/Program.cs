e6f254e0f28fd25fb145effe6c574ad699501481
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-15 11:46:56 +0200

    chore: add extract capitals from a string logic

---
+++ ExtactCapitals/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace ExtactCapitals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        // Extact capitals letters from a string
+
+        public static string ExtractCapitals(string? input)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < input?.Length; i++) 
+            {
+                char c = input[i];
+                if (char.IsUpper(c))
+                {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
