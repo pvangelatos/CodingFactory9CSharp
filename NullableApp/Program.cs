a781bc4d8c373aec9e025f81b856674183efa151
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-15 11:47:13 +0200

    chore: add nullable strings

---
+++ NullableApp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace NullableApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? s = Console.ReadLine();     // possible null value
+
+            // Σωστός χειρισμός της πιθανότητας null με έλεγχο
+            if (s != null) Console.WriteLine(s.Length); // null check
+
+            Console.WriteLine(s?.Length);       // null-conditional operator
+            Console.WriteLine(s!.Length);       // null-forgiving operator (use with caution)
+            Console.WriteLine(s ?? "default");  // null-coalescing operator
+        }
+    }
+}
