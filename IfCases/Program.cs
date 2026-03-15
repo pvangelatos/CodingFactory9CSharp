42682a23c3c8ce3b8b4f166db3459930f95ed71e
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-15 10:41:06 +0200

    chore: add if cases

---
+++ IfCases/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace IfCases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 20;
+            string name = null;
+            string nickname = "Ally";
+
+            if (age > 18)
+            {
+                Console.WriteLine("Ενήλικας");
+            }
+            else
+            {
+                Console.WriteLine("Ανήλικος");
+            }
+
+            var status = age > 18 ? "Ενήλικας" : "Ανήλικος";
+            Console.WriteLine(status);  // Ενήλικας
+
+            var displayName = name ?? "Άγνωστος";
+            Console.WriteLine(displayName);
+
+            var length = name?.Length ?? 0;
+            Console.WriteLine(length);
+        }
+    }
+}
