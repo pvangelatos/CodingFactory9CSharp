ab9db6d6bdbe7a409ff2b19c365310036e5c07f3
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-15 10:42:52 +0200

    chore: add switch examples with classic switch case and switch expression and switch pattern matching

---
+++ SwitchApp/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace SwitchApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dayOfWeek = "Monday";
+
+            switch (dayOfWeek)
+            {
+                case "Monday":
+                    Console.WriteLine("It's Monday!");
+                    break;
+                case "Tuesday":
+                    Console.WriteLine("It's Tuesday!");
+                    break;
+                case "Wednesday":
+                    Console.WriteLine("It's Wednesday!");
+                    break;
+                case "Thursday":
+                    Console.WriteLine("It's Thursday!");
+                    break;
+                case "Friday":
+                    Console.WriteLine("It's Friday!");
+                    break;
+                case "Saturday":
+                    Console.WriteLine("It's Saturday!");
+                    break;
+                case "Sunday":
+                    Console.WriteLine("It's Sunday!");
+                    break;
+                default:
+                    Console.WriteLine("Invalid day of the week.");
+                    break;
+            }
+
+            // Switch expression example
+            int day = 2;
+
+            var result = day switch
+            {
+                1 => "Monday",
+                2 => "Tuesday",
+                3 => "Wednesday",
+                4 => "Thursday",
+                5 => "Friday",
+                6 => "Saturday",
+                7 => "Sunday",
+                _ => "Invalid day"
+            };
+
+            // pattern matching example with switch expression
+            int score = 85;
+            
+            var grade = score switch
+            {
+                >= 90 => "A",
+                >= 80 => "B",
+                >= 70 => "C",
+                >= 50 => "D",
+                _ => "F"
+            };
+
+
+        }
+    }
+}
