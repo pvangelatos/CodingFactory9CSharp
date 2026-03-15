0563fc08253725b241153a2d1ed963c8cd488781
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-15 10:43:12 +0200

    chore: add while example

---
+++ WhileApp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace WhileApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int END = 10;
+            int sum = 0;
+            int i = 1;
+
+            while (i <= END)
+            {
+                sum += i;
+                i++;
+            }
+
+            Console.WriteLine($"Το άθροισμα από 1 έως {END} είναι: {sum}");
+
+        }
+    }
+}
