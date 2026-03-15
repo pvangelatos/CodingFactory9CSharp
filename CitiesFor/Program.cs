ffdb424abd1035324a302cab4728aa7249388ae5
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-15 10:39:32 +0200

    chore: add iterative logic

---
+++ CitiesFor/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CitiesFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = { "Athens", "Thessaloniki", "Patras", "Heraklion", "Larissa" };
+
+            for (int i = 0; i < cities.Length; i++)
+            {
+                Console.WriteLine(cities[i]);
+            }
+
+            foreach (string city in cities)
+            {
+                Console.WriteLine(city);
+            }
+        }
+    }
+}
