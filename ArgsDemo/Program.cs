950047007dcf5f4201d2be55dce53858ad4c08d8
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-15 18:49:17 +0200

    chore: add args logic for cli input through parameters

---
+++ ArgsDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ArgsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           if (args.Length !=3)
+            {
+                Console.WriteLine("Error in CLI");
+                Environment.Exit(1);    // Exit with error code    
+            }
+
+            Console.WriteLine($"{args[0]}, {args[1]}, {args[2]}");
+        }
+    }
+}
