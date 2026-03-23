ed40436095a254de256a4f4bcda9b4a6a48ed3a8
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-23 11:16:27 +0200

    chore: add Point2D that extends Point

---
+++ PointApp/Model/Point2D.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PointApp.Model
+{
+    internal class Point2D : Point
+    {
+        public int Y { get; set; }
+
+        public Point2D() // : base()
+        {
+            // Y = 0;
+        }
+
+        public Point2D(int x, int y) : base(x)
+        {
+            Y = y;
+        }
+
+        public override string ToString() => $"X={X}, Y={Y}";
+
+        public override void Move5()
+        {
+            base.Move5();
+            Y += 5;
+        }
+    }
+}
