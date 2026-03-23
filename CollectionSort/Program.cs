9c12a57c91123b54606a0e261ed578141b8d1b4c
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-23 17:21:40 +0200

    chore: add sorting logic with sort, IComparable and interface IComparer

---
+++ CollectionSort/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CollectionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students =
+                 [
+                     new Student() { Firstname = "Alice", Lastname = "A.", Age = 22 },
+                    new Student() { Firstname = "Bob", Lastname = "B.", Age = 20 }
+                 ];
+
+            students.Sort();            // Χρησιμοποιεί την compareTo
+            students.Sort((s1, s2) => string.Compare(s1.Lastname, s2.Lastname, StringComparison.Ordinal));    // IComparer
+        }
+    }
+}
---
+++ CollectionSort/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectionSort
+{
+    internal class Student : IComparable<Student>
+    {
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public int Age { get; set; }
+
+        public int CompareTo(Student? other)
+        {
+            return string.Compare(Lastname, other?.Lastname, StringComparison.Ordinal); // null safe
+        }
+    }
+}
