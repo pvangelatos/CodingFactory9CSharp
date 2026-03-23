236d5c5a0151eb1d6745fb5239f8b2b1c742ec80
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-23 10:26:16 +0200

    chore: add account app and utility

---
+++ AccountApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AccountApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ UtilityApp/HelloUtil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UtilityApp
+{
+    internal static class HelloUtil
+    {
+        public static void SayHello()
+        {
+            Console.WriteLine($"Hello, World!");
+        }
+    }
+}
