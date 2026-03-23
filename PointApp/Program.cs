78a0520631e26a94da59ec91392bb30bafcbcfb7
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-23 11:17:27 +0200

    chore: add polymorphism and usage

---
+++ PointApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using PointApp.Model;
+
+namespace PointApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new(15);
+            Point p2 = new Point2D(15, 18);
+            Point p3 = new Point3D(15, 18, 20);
+
+            DoMove5(p1);
+            DoMove5(p2);
+            DoMove5(p3);
+
+            DoPrint(p1);
+            DoPrint(p2);
+            DoPrint(p3);
+        }
+
+        public static void DoMove5(Point p)
+        {
+            p.Move5();
+        }
+
+        public static void DoPrint(Point p)
+        {
+            Console.WriteLine(p.ToString());
+        }
+    }
+}
