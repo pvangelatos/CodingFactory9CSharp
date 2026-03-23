0b24343c271d15a36665e88ef87e22c3b1d8de02
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-23 11:16:40 +0200

    chore: add point3d logic

---
+++ PointApp/Model/Point3D.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PointApp.Model
+{
+    internal class Point3D : Point2D
+    {
+        public int Z { get; set; }
+
+        public Point3D()
+        {
+        }
+
+        public Point3D(int x, int y, int z) : base(x, y)
+        {
+            Z = z;
+        }
+
+        public override string ToString() => $"X={X}, Y={Y}, Z={Z}";
+
+        public override void Move5()
+        {
+            base.Move5();
+            Z += 5;
+        }
+    }
+}
