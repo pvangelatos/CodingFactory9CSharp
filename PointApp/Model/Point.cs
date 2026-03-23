59f5bb8947055d08a57642a4919339cdb028434a
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-23 11:16:12 +0200

    chore: add point class

---
+++ PointApp/Model/Point.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PointApp.Model
+{
+    internal class Point
+    {
+        public int X { get; set; }
+
+        public Point()
+        {
+
+        }
+
+        public Point(int x)
+        {
+            X = x;
+        }
+
+        public override string ToString() => $"X={X}";
+
+        public virtual void Move5()
+        {
+            X += 5;
+        }
+    }
+}
