dd1e5e0da138440f12ccf71710f569f3e2480f32
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-23 12:27:18 +0200

    chore: add abstract class logic

---
+++ AbstractApp/AbstractAnimal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractApp
+{
+    internal abstract class AbstractAnimal
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public double Age { get; set; }
+
+        public abstract override string ToString();
+
+        public virtual void Eat()
+        {
+            Console.WriteLine($"{Name} is eating.");
+        }
+    }
+}
---
+++ AbstractApp/Cat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace AbstractApp
+{
+    internal class Cat : AbstractAnimal
+    {
+        public override void Eat()
+        {
+            base.Eat();
+            Console.WriteLine($"{Name} is finally eating.");
+        }
+        public override string ToString() => $"Id={Id}, Name={Name}, Age={Age}";
+    }
+}
