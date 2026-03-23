bdc5f0511c18640a310b467fc525b8876c012cbb
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-23 12:27:32 +0200

    chore: add copy constructors

---
+++ CopyApp/Address.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CopyApp
+{
+    internal class Address
+    {
+        public string? Street { get; set; }
+        public string? Number { get; set; }
+        public string? ZipCode { get; set; }
+    }
+}
---
+++ CopyApp/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CopyApp
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public Address? Address { get; set; }
+
+        // Copy constructor
+        public Student(Student student)
+        {
+            Id = student.Id;
+            Firstname = student.Firstname;
+            Lastname = student.Lastname;
+            Address = new Address
+            {
+                Street = student.Address?.Street,
+                Number = student.Address?.Number,
+                ZipCode = student.Address?.ZipCode
+            };
+        }
+    }
+}
