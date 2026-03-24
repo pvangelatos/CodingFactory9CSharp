71f052235737b5ba451ae42817a5ca04769966ff
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-24 16:10:25 +0200

    chore: add file management logic

--- FileManagement/Program.cs
+++ FileManagement/Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DLList<char> dll = new();
+            ListNode<char>? node;
+
+            string filePath = @"C:\tmp\file.txt";
+            int ordinal;
+            char ch;
+            int totalChars = 0;
+
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    Console.WriteLine($"File not found: {filePath}");
+                    return;
+                }
+
+                using StreamReader reader = new(filePath);
+
+                while ((ordinal = reader.Read()) != -1)
+                {
+                    ch = (char)ordinal;
+                    if (ch is '\r' or '\n') continue;
+
+                    node = dll.FindNode(ch);
+                    if (node is null)
+                    {
+                        dll.InsertLast(ch);
+                    }
+                    else
+                    {
+                        dll.IncreaseCount(ch);
+                    }
+                    totalChars++;
+                }
+                dll.SortByCount();
+                dll.Traverse(totalChars);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
