f16dfeba7f3b169640a57f3087313e323ffa8825
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-26 15:04:50 +0200

    chore: add student model with post

--- WebAppStarter9/Pages/Index.cshtml.cs
+++ WebAppStarter9/Pages/Index.cshtml.cs
@@ -1,13 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
 
 namespace WebAppStarter9.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        public string? CurrentDay { get; set; }
+        private readonly ILogger<IndexModel> _logger;
 
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult OnGet()
+        {
+            CurrentDay = DateTime.Now.ToString("dddd", CultureInfo.InvariantCulture);
+            return Page();
         }
     }
 }
