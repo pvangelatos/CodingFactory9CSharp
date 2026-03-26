d8a2e7f7ded8a0329d385ad8368eb438e5b24361
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-26 10:30:32 +0200

    chore: add dto for insert student

---
+++ WebAppStarter9/DTO/StudentReadOnlyDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAppStarter9.DTO
+{
+    public record StudentReadOnlyDTO(int Id, string? Firstname, string? Lastname)
+    {
+            public StudentReadOnlyDTO() : this(default, default, default)
+            { }
+    }
+}
