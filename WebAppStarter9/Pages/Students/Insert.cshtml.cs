354d2109cbb259d738bc6f7ba0a91071450b321a
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-26 10:31:37 +0200

    chore: add index controller

---
+++ WebAppStarter9/Pages/Students/Insert.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppStarter9.Pages.Students
+{
+    public class InsertModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
